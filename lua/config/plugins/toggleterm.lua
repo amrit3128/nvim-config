f5dcee46e8d301609afad0ef260f13e6b5eb348f
Amritanshu Tripathy <tripathyamritanshu7@gmail.com>
2024-12-20 00:15:54 +0530

    lazygit and toggleterm

---
+++ lua/config/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  -- amongst your other plugins
+  {
+    'akinsho/toggleterm.nvim',
+    enabled = false,
+    version = "*",
+    config = true
+  }
+  -- or
+  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+}
